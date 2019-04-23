f98cfbc7bdb55386e2a8b681dae00e5c60d18e4e
Prarthana Sundar <ps@stripe.com>
2019-04-23 16:41:17 -0700

    stripe foundation class store

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ function purchaseProduct(event) {
 function openProductModal() {
   $('#purchaseProductModal').modal('show')
 }
+
+$(function() {
+  $('.card-img-top').on('click', function() {
+    console.log($(this))
+    $('.imagepreview').attr('src', $(this).attr('src'));
+    $('#imagemodal').modal('show');   
+  });		
+});
